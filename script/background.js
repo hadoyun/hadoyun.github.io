fc7951902dafeb9b4b001161fea06ce27bcca90c
hadoyun <48614550+hadoyun178@users.noreply.github.com>
2021-11-26 20:07:24 +0900

    createElement 메소드를 이용한 html 태그 추가

--- script/background.js
+++ script/background.js
@@ -5,6 +5,11 @@ const backgrondImage = [
     "04.jpeg"
 ];
 
-function paintBackground(){
-    
-}
+const chosenImage = backgrondImage[Math.floor(Math.random()*backgrondImage.length)];
+
+//어디에도 없는 태그 생성!
+const bgImage = document.createElement("img");
+// chosenImage를 이용해 랜덤하게 주소 생성
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
