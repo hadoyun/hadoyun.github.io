2276701732e2c09246720950ecc06baa84b44ca6
hadoyun <48614550+hadoyun178@users.noreply.github.com>
2021-11-26 18:18:44 +0900

    함수 이름 변경 / Element 변경

--- script/greeting.js
+++ script/greeting.js
@@ -1,21 +1,24 @@
-const input = document.querySelector("#login-form input");
-const inputbts = document.querySelector("#login-form button");
+
+const loginform = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+//submit 타입을 주면 찾을 필요가 없다.
+//const inputbts = document.querySelector("#login-form button");
 
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN = "hidden";
 
-const USERNAME = input.value;
+const USERNAME = loginInput.value;
 
-function loging(event){
+function onSubmit(event){
     //새로고침 방지
-    // event.preventDefault();
+    event.preventDefault();
     // localStorage.setItem("유저이름", input.value);
     // input.classList.add(HIDDEN);
     // greeting.classList.remove(HIDDEN);
 }
 
-inputbts.addEventListener("click", loging);
-// window.addEventListener("submit", loging);
+//window.addEventListener("submit", onSubmit);
 
-//inputbts.addEventListener("click", loging);
+//submit 은 엔터나 클릭 둘다 반응한다.
+loginform.addEventListener("submit", onSubmit);
