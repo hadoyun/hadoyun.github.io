87e950bd1967b1716a3b9d2d80ff950980f80330
hadoyun <48614550+hadoyun178@users.noreply.github.com>
2021-11-26 17:05:13 +0900

    오타 변경

--- script/greeting.js
+++ script/greeting.js
@@ -5,12 +5,24 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN = "hidden";
 
+const USERNAME = input.value;
+
 function loging(event){
     //새로고침 방지
-    event.preventDefault();
-    localStorage.setItem("유저이름", input.value);
-    input.classList.add(HIDDEN);
-    greeting.classList.remove(HIDDEN);
+    // event.preventDefault();
+    // localStorage.setItem("유저이름", input.value);
+    // input.classList.add(HIDDEN);
+    // greeting.classList.remove(HIDDEN);
+    
+    //유효성 검사
+    if(USERNAME === ""){
+        alert("이름이 없어용!");
+    }
 }
 
-window.addEventListener("submit", loging);
+//""도 검색 가능
+
+inputbts.addEventListener("click", loging);
+// window.addEventListener("submit", loging);
+
+//inputbts.addEventListener("click", loging);
