8a062bddcf26fa3543435d90df78b3b2f1a195c3
hadoyun <55866690+hadoyun@users.noreply.github.com>
2021-11-28 04:29:30 +0900

    fetch
    url

--- script/weather.js
+++ script/weather.js
@@ -1,5 +1,16 @@
+const API_KEY = "b953e4213199662a18d4e7fb2b01b624";
+
 function onGeoOk(position){
-    console.log(position);
+    const lat = position.coords.latitude;
+    const log = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+    // ???
+    fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+        console.log(data.name, data.weather[0].main);
+    });
+
 }
 
 function onGeoError(){
